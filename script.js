53b4f818c696115a3711faae9a578aea83ef4e6e
Omkar Jadhav <48726860+bLAckBLAZER@users.noreply.github.com>
2021-08-21 15:58:23 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var inputText = document.querySelector("#input-text");
+var btnTranslate = document.querySelector("#btn-translate");
+var outputText = document.querySelector("#output-text");
+
+const serverUrl = "https://api.funtranslations.com/translate/klingon.json"
+// const serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+// console.log(btnTranslate);
+// console.log(outputText);
+
+
+function generateUrl(inputText) {
+    var convertedUrl = serverUrl + "?text=" + inputText;
+    return convertedUrl;
+}
+
+function errorHandler(error) {
+    alert("Sorry! Some issue with server...try again after sometime");
+}
+
+function translateToKlingon() {
+
+    fetch(generateUrl(inputText.value))
+        .then(response => response.json())
+        .then(json => {
+            outputText.innerHTML = json.contents.translated
+        })
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", translateToKlingon);
